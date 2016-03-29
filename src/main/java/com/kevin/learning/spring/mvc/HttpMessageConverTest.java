c7f43ba7e98f88dcf8290d2989e1ecbda4f2a45f
kevin <googlevsbing@126.com>
2016-03-29 17:37:21 +0800

    提交源码分析博客地址

--- src/main/java/com/kevin/learning/spring/mvc/HttpMessageConverTest.java
+++ src/main/java/com/kevin/learning/spring/mvc/HttpMessageConverTest.java
@@ -4,10 +4,16 @@ import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.scheduling.config.AnnotationDrivenBeanDefinitionParser;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
+/**
+ * http://www.cnblogs.com/fangjian0423/p/springMVC-xml-json-convert.html
+ * @author Administrator
+ *
+ */
 public class HttpMessageConverTest {
 
 	public static void test1(){
@@ -21,5 +27,9 @@ public class HttpMessageConverTest {
 		HandlerMethodReturnValueHandler res2;
 		
 		RequestResponseBodyMethodProcessor p;
+		
+		//解析 <mvc:annotation-driven/>
+		AnnotationDrivenBeanDefinitionParser parser;
+		
 	}
 }
